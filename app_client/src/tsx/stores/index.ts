288b12787f9a2058c7b54232ac44d55c79ec9e29
daichi1123 <badass.haruzion@gmail.com>
2022-04-19 09:31:51 +0900

    feat: store file for slices

---
+++ app_client/src/tsx/stores/index.ts
@@ -0,0 +1,17 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
+import authReducer from "./slices/authSlice"
+export const store = configureStore ({
+  reducer: {
+    // storeに登録完了 redux dev toolsで登録されているかの確認が可能
+    auth: authReducer,
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+export type AppDispatch = typeof store.dispatch;
