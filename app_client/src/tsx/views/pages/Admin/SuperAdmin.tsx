95e77a0f926c0a969dafaf89f20766a4b1a14fd0
daichi1123 <badass.haruzion@gmail.com>
2022-04-19 09:33:28 +0900

    feat: add super admin bese page

---
+++ app_client/src/tsx/views/pages/Admin/SuperAdmin.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Link } from "react-router-dom";
+
+function SuperAdmin() {
+  return (
+    <div>
+      <Link to="/">リンクテキスト</Link>
+    </div> 
+  );
+}
+
+
+export default SuperAdmin
