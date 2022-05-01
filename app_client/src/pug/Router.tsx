7dfd3c99b93bcd4adf4bda2700ffadd523f6274d
daichi1123 <badass.haruzion@gmail.com>
2022-05-01 23:55:46 +0900

    feat: add router

--- app_client/src/pug/Router.tsx
+++ app_client/src/pug/Router.tsx
@@ -1,28 +1,43 @@
 import React from "react";
 // react router domは、5系で使用するべき
 import { Route, BrowserRouter, Switch } from "react-router-dom";
-import Index from "../tsx/views/pages/Client/Index";
-import Map from "../tsx/views/pages/Client/Map";
 import Owner from "../tsx/views/pages/Owner/Owner";
+
+import CreateGenre from "../tsx/views/pages/Admin/CreateGenre";
+import Dashboard from "../tsx/views/pages/Admin/Dashboard";
+import AdminSignIn from "../tsx/views/pages/Admin/AdminSignin";
+
+import Index from "../tsx/views/pages/Client/Index";
+// Google Mapsに使用する
+import Map from "../tsx/views/pages/Client/Map";
+import SearchRestrant from "../tsx/views/pages/Client/SearchRestrant";
+
 import Coupon from "../tsx/views/pages/Owner/Coupon";
 import RegisterRestrant from "../tsx/views/pages/Owner/RegistRestrant";
-import SearchRestrant from "../tsx/views/pages/Client/SearchRestrant";
-import SuperAdmin from "../tsx/views/pages/Admin/SuperAdmin";
+import AuthGoogleByFirebase from "../tsx/views/pages/Owner/OwnerSignin";
 
 
 function Router() {
   return (
     <BrowserRouter>
       <Switch>
+        {/* Client */}
         <Route exact path="/" component={Index} />
-        {/* GoogleMap APIを使用して作成していく */}
         <Route exact path="/map" component={Map} />
         <Route exact path="/search" component={SearchRestrant} />
+
+        {/* Owner */}
+        <Route exact path="/owner/signin" component={AuthGoogleByFirebase} />
+        <Route exact path="/owner/coupons/create" component={Coupon} />
+        <Route exact path="/owner/restaurants/register" component={RegisterRestrant} />
+
+        {/* Admin */}
+        <Route exact path="/admin/signin" component={AdminSignIn} />
+        <Route exact path="/admin/genres/create" component={CreateGenre} />
+        <Route exact path="/admin/dashboard" component={Dashboard} />
+        
+        {/* Test */}
         <Route exact path="/owner" component={Owner} />
-        <Route exact path="/SuperAdmin" component={SuperAdmin} />
-        {/* Ownerのページ */}
-        <Route exact path="/coupon" component={Coupon} />
-        <Route exact path="/regist/restrant" component={RegisterRestrant} />
       </Switch>
     </BrowserRouter>
   );
