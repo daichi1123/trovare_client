f46890e8b6be16f2dcd3130086adf266f0bbc933
daichi1123 <badass.haruzion@gmail.com>
2022-05-05 00:37:55 +0900

    feat: textfields value

---
+++ app_client/src/tsx/utils/Form/TextArea.tsx
@@ -0,0 +1,20 @@
+export const TextFields = [
+  {
+    id: "restaurant-name",
+    labelName: "レストランの名前*",
+    type: "text",
+    autoComplete: "",
+  },
+  {
+    id: "zip-code",
+    labelName: "郵便番号*",
+    type: "text",
+    autoComplete: "",
+  },
+  {
+    id: "address",
+    labelName: "住所*",
+    type: "text",
+    autoComplete: "",
+  },
+];
