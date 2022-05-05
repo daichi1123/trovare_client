422ea0b99a2bbf9b095330ae00bb6359ca832d46
daichi1123 <badass.haruzion@gmail.com>
2022-05-05 17:51:52 +0900

    feat: add top page

---
+++ app_client/src/tsx/views/pages/Client/Top.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/block/Header";
+
+
+export const Top = () => {
+  // 使用するコンポーネント Header picture card search 
+  return (
+    <>
+      <Header/>
+    </>
+  );
+}
