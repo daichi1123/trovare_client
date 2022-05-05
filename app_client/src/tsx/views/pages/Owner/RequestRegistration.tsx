1ad7de351bd83bd1f34bc8545631bb39325478fe
daichi1123 <badass.haruzion@gmail.com>
2022-05-05 17:52:30 +0900

    feat: add request registration page

---
+++ app_client/src/tsx/views/pages/Owner/RequestRegistration.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/block/Header";
+import { RequestRegistrationForm } from "../../components/block/RequestRegistrationForm";
+
+export const RequestRegistration = () => {
+  return (
+    <>
+      <Header />
+      <RequestRegistrationForm />
+    </>
+  );
+};
