49ec45fdd653592bb1a622f1ee93485289ff6f39
daichi1123 <badass.haruzion@gmail.com>
2022-05-07 02:24:47 +0900

    delete: no need lib and add code for confirming

---
+++ app_client/src/tsx/views/pages/Client/SaveRestaurants.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "../../components/block/Header";
+
+export const SaveRestaurants = () => {
+  return (
+    <>
+      <Header/>
+      <h1>SaveRestaurant Page</h1>
+    </> 
+  );
+}
