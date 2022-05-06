3654cd3b9d727bd03950b10fb603408dc192c7ee
daichi1123 <badass.haruzion@gmail.com>
2022-05-07 01:06:02 +0900

    feat: add new components

--- app_client/src/tsx/views/pages/Client/Top.tsx
+++ app_client/src/tsx/views/pages/Client/Top.tsx
@@ -1,11 +1,14 @@
 import { Header } from "../../components/block/Header";
-
+import { ListTabs } from "../../components/block/ListTabs";
+import { SearchCard } from "../../components/block/SearchCard";
 
 export const Top = () => {
-  // 使用するコンポーネント Header picture card search 
+  // 使用するコンポーネント Header picture card search
   return (
     <>
-      <Header/>
+      <Header />
+      <SearchCard />
+      <ListTabs />
     </>
   );
-}
+};
