ba7af52b21565de5d4300217c149484bab40e47a
daichi1123 <badass.haruzion@gmail.com>
2022-05-07 02:24:37 +0900

    delete: no need lib and add code for confirming

--- app_client/src/tsx/views/pages/Client/Map.tsx
+++ app_client/src/tsx/views/pages/Client/Map.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Header } from "../../components/block/Header";
-import MapHeader from "../../components/block/MapHeader";
 
 const Map = () => {
   return (
     <>
       <Header/>
+      <h1>Map Page</h1>
     </> 
   );
 }
