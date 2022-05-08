90c6c222a78027b75d512ce7bcb4b48b129d8e5e
daichi1123 <badass.haruzion@gmail.com>
2022-05-08 22:13:49 +0900

    feat: fomat code and google api key

--- app_client/src/tsx/utils/const.tsx
+++ app_client/src/tsx/utils/const.tsx
@@ -1,14 +1,16 @@
 // Application
-export const ApplicationTitle = process.env.REACT_APP_TITLE
-export const ApiURL = process.env.REACT_APP_DEV_API_URL
+export const ApplicationTitle = process.env.REACT_APP_TITLE;
+export const ApiURL = process.env.REACT_APP_DEV_API_URL;
+export const GoogleMapApiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 
 // firebase
-export const FirebaseApiKey = process.env.REACT_APP_FIREBASE_APIKEY
-export const FirebaseDomain = process.env.REACT_APP_FIREBASE_DOMAIN
-export const FirebaseDatabase = process.env.REACT_APP_FIREBASE_DATABASE
-export const FirebaseProjectID = process.env.REACT_APP_FIREBASE_PROJECT_ID
-export const FirebaseStorageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
-export const FirebaseSenderID = process.env.REACT_APP_FIREBASE_SENDER_ID
-export const FirebaseAppID = process.env.REACT_APP_FIREBASE_APP_ID
+export const FirebaseApiKey = process.env.REACT_APP_FIREBASE_APIKEY;
+export const FirebaseDomain = process.env.REACT_APP_FIREBASE_DOMAIN;
+export const FirebaseDatabase = process.env.REACT_APP_FIREBASE_DATABASE;
+export const FirebaseProjectID = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+export const FirebaseStorageBucket =
+  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+export const FirebaseSenderID = process.env.REACT_APP_FIREBASE_SENDER_ID;
+export const FirebaseAppID = process.env.REACT_APP_FIREBASE_APP_ID;
 
 // AWS
